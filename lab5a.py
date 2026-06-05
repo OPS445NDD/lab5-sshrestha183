6a96b1178b3161126aed4a155c8f233a7f0af867
sshrestha183 <sshrestha183@myseneca.ca>
2026-06-05 09:20:15 -0400

    Completed lab5

---
+++ lab5a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def read_file_string(file_name):
+    f = open(file_name, 'r')
+    data = f.read()
+    f.close()
+    return data
+
+def read_file_list(file_name):
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    f.close()
+
+    result = []
+
+    for line in lines:
+        result.append(line.strip('\n'))
+
+    return result
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
